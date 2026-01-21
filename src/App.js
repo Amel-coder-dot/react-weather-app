f7c899dc9cef7f4cfd55f2241f2148b79c0dddba
Amel-coder-dot <collie.freya@gmail.com>
2026-01-21 11:04:43 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Amel-coder-dot" target="_blank">
+          Freya Collie
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Amel-coder-dot/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
