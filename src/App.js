eee7129773f6bd758b3c48b224d1f17eb2c14da6
Amel-coder-dot <collie.freya@gmail.com>
2026-01-21 11:28:55 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/Amel-coder-dot"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Freya Collie
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Amel-coder-dot/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Amel-coder-dot"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Freya Collie
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Amel-coder-dot/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
